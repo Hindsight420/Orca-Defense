0e2d0d15bc69c9a4154657cb0e39ba06281a0925
Milan <milan.andries@live.be>
2022-04-12 18:14:14 +0200

    I don't even know anymore

---
+++ Assets/_Scripts/1Data/Scriptables/FishingBuilding.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Building Type/Fishing Building")]
+public class FishingBuilding : BuildingType
+{
+    public float FishRate;
+}
