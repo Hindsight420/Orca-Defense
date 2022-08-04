c9d99cd81e6ddf1f4ce66c4ee92d9429347bae3e
Jared Irwin <Jared.irwinw@yahoo.com>
2022-08-04 19:10:14 +0100

    penguin prefab

---
+++ Assets/_Scripts/2Models/PenguinBody.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinBody : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer WingRight;
+    [SerializeField]
+    private SpriteRenderer WingLeft;
+    [SerializeField]
+    private SpriteRenderer LegLeft;
+    [SerializeField]
+    private SpriteRenderer LegRight;
+    [SerializeField]
+    private SpriteRenderer FootLeft;
+    [SerializeField]
+    private SpriteRenderer FootRight;
+    [SerializeField]
+    private SpriteRenderer EyeLeft;
+    [SerializeField]
+    private SpriteRenderer EyeRight;
+    [SerializeField]
+    private SpriteRenderer PupilLeft;
+    [SerializeField]
+    private SpriteRenderer PupilRight;
+    [SerializeField]
+    private SpriteRenderer BeakTop;
+    [SerializeField]
+    private SpriteRenderer BeakBottom;
+    [SerializeField]
+    private SpriteRenderer Head;
+    [SerializeField]
+    private SpriteRenderer Body;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
